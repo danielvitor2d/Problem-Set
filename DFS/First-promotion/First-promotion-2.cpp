0599aaed7c07852937c7e4cf631f1759904d14f4
Daniel Vitor <ripsea.rpg321@gmail.com>
2020-10-12 22:59:17 -0300

    Added solution for First Promotion

---
+++ DFS/First-promotion/First-promotion-2.cpp
@@ -0,0 +1,85 @@
+/*
+	Author: [UFC-QXD] Daniel Vitor Pereira Rodrigues <danielvitor@alu.ufc.br>
+	Problem: First Promotion
+	Link: https://neps.academy/problem/823
+	Origin: OBI 2020 - First Phase - Level 2
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long i64;
+typedef pair<int, int> ii;
+typedef pair<i64, i64> ll;
+typedef vector<int> vi;
+typedef vector<i64> vi64;
+typedef vector<ii> vii;
+typedef vector<ll> vll;
+typedef vector<vi> vvi;
+
+const double eps = 1e-9;
+
+#define eq(a, b) (abs(a - b) < eps)
+#define lt(a, b) ((a + eps) < b)
+#define gt(a, b) (a > (b + eps))
+#define le(a, b) (a < (b + eps))
+#define ge(a, b) ((a + eps) > b)
+
+#define fastIO() ios_base::sync_with_stdio(0), cin.tie(0)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define ms(a, x) memset(a, x, sizeof(a))
+#define len(x) (x).size()
+#define pb push_back
+#define eb emplace_back
+#define fi first
+#define se second
+
+const int dtx[] = { 0, 0, -1, 1, 1, -1,  1, -1};
+const int dty[] = {-1, 1,  0, 0, 1, -1, -1,  1};
+const int dtxc[] = {1,  1, 2,  2, -1, -1, -2, -2};
+const int dtyc[] = {2, -2, 1, -1,  2, -2,  1, -1};
+
+const double pi = acos(-1.0);
+const int inf = 0x3f3f3f3f;
+const int maxn = 5e4+5;
+const int mod = 1e9+7;
+
+int n, a, b, e, res;
+vii gr[maxn];
+
+ii dfs(int u, int p) {
+	ii ans(0, 0);
+
+	for (ii edge : gr[u]) {
+		int to = edge.fi, cl = edge.se;
+		if (to == p) continue;
+		ii rs = dfs(to, u);
+		if (cl == 0) {
+			ans.fi = max(ans.fi, rs.se + 1);
+		} else {
+			ans.se = max(ans.se, rs.fi + 1);
+		}
+	}
+
+	res = max(res, ans.fi + ans.se + 1);
+
+	return ans;
+}
+
+int main() {
+	fastIO();
+	
+	cin >> n;
+
+	for (int i = 0; i < n-1; ++i) {
+		cin >> a >> b >> e;
+		gr[a].eb(b, e);
+		gr[b].eb(a, e);
+	}
+
+	dfs(1, 0);
+
+	cout << res << '\n';
+
+	return 0;
+}
---
+++ DFS/README.md
@@ -0,0 +1,6 @@
+# DFS (Depth First Search)
+
+## DFS + DP
+Problem | Solution
+------- | --------
+[First Promotion](https://neps.academy/problem/823) | [Link to the solution](https://github.com/danielvitor2d/Problem-Set/blob/main/DFS/First-promotion/First-promotion.cpp)
